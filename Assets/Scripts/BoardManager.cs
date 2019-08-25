9ce2079881778588389430089f91ff4c7f666c67
Anderson Contreras <alexandearko@gmail.com>
2019-08-25 13:35:05 -0400

    Agregado la deteccion de 3 en filas. Se eleminan 3 en fila. Caen nuevos objetos

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -8,14 +8,16 @@ public class BoardManager : MonoBehaviour
     public List<Sprite> prefabs = new List<Sprite>();
     public GameObject currentCandy;
     public int xSize, ySize;
-    
+
     private GameObject[,] candies;
 
-    public bool isShifting {get; set;}
+    public bool isShifting { get; set; }
+    private Candy selectedCandy;
+    public const int MinCandiesToMatch = 2;
     // Start is called before the first frame update
     void Start()
     {
-        if(sharedInstance == null)
+        if (sharedInstance == null)
         {
             sharedInstance = this;
         }
@@ -37,18 +39,18 @@ public class BoardManager : MonoBehaviour
 
         int idx = -1;
 
-        for(int x = 0; x < xSize; x++)
+        for (int x = 0; x < xSize; x++)
         {
-            for(int y = 0; y < ySize; y++)
+            for (int y = 0; y < ySize; y++)
             {
-                GameObject newCandy = Instantiate(currentCandy, new Vector3(startX + (offset.x*x), startY + (offset.y*y), 0), currentCandy.transform.rotation);
-                newCandy.name = string.Format("Candy[{0}] [{1}]" , x, y);
+                GameObject newCandy = Instantiate(currentCandy, new Vector3(startX + (offset.x * x), startY + (offset.y * y), 0), currentCandy.transform.rotation);
+                newCandy.name = string.Format("Candy[{0}] [{1}]", x, y);
 
                 do
                 {
-                    idx = Random.Range(0, prefabs.Count);      
+                    idx = Random.Range(0, prefabs.Count);
                 }
-                while((x>0 && idx == candies[x-1,y].GetComponent<Candy>().id) || (y>0 && idx == candies[x,y-1].GetComponent<Candy>().id));
+                while ((x > 0 && idx == candies[x - 1, y].GetComponent<Candy>().id) || (y > 0 && idx == candies[x, y - 1].GetComponent<Candy>().id));
 
                 Sprite sprite = prefabs[idx];
                 newCandy.GetComponent<SpriteRenderer>().sprite = sprite;
@@ -59,9 +61,66 @@ public class BoardManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator FindNullCandies()
     {
-        
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                if (candies[x, y].GetComponent<SpriteRenderer>().sprite == null)
+                {
+                    yield return StartCoroutine(MakeCandiesFall(x, y));
+                    break;
+                }
+            }
+        }
+
+    }
+    private IEnumerator MakeCandiesFall(int x, int yStart, float shiftDelay = 0.05f)
+    {
+        isShifting = true;
+        List<SpriteRenderer> renderes = new List<SpriteRenderer>();
+        int nullCandies = 0;
+
+        for (int y = yStart; y < ySize; y++)
+        {
+            SpriteRenderer spriteRenderer = candies[x, y].GetComponent<SpriteRenderer>();
+            if (spriteRenderer.sprite == null)
+            {
+                nullCandies++;
+            }
+            renderes.Add(spriteRenderer);
+        }
+        for (int i = 0; i < nullCandies; i++)
+        {
+            yield return new WaitForSeconds(shiftDelay);
+            for (int j = 0; j < renderes.Count - 1; j++)
+            {
+                renderes[j].sprite = renderes[j + 1].sprite;
+                renderes[j + 1].sprite = GetNewCandie(x, ySize-1);
+            }
+        }
+
+
+        isShifting = false;
+    }
+    private Sprite GetNewCandie(int x, int y)
+    {
+        List<Sprite> possibleCandies = new List<Sprite>();
+        possibleCandies.AddRange(prefabs);
+
+        if (x > 0)
+        {
+            possibleCandies.Remove(candies[x - 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (x < xSize - 1)
+        {
+            possibleCandies.Remove(candies[x + 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (y > 0)
+        {
+            possibleCandies.Remove(candies[x, y - 1].GetComponent<SpriteRenderer>().sprite);
+        }
+        return possibleCandies[Random.Range(0, possibleCandies.Count)];
     }
 }
--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -23,15 +23,147 @@ public class Candy : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void SelectCandy()
     {
-        
+        isSelected = true;
+        spriteRenderer.color = selectedColor;
+        previousSelected = gameObject.GetComponent<Candy>();
+    }
+    private void DeselectCandy()
+    {
+        isSelected = false;
+        spriteRenderer.color = Color.white;
+        previousSelected = null;
+    }
+    private void OnMouseDown()
+    {
+        if(spriteRenderer.sprite == null || BoardManager.sharedInstance.isShifting)
+        {
+            return;
+        }
+        if(isSelected)
+        {
+            DeselectCandy();
+        }
+        else
+        {
+            if(previousSelected == null)
+            {
+                SelectCandy();
+            }
+            else
+            {
+                if(CanSwipe())
+                {
+                SwapSprite(previousSelected);
+                previousSelected.FindAllMatches();
+                previousSelected.DeselectCandy();
+                FindAllMatches();
+
+                StopCoroutine(BoardManager.sharedInstance.FindNullCandies());
+                StartCoroutine(BoardManager.sharedInstance.FindNullCandies());
+                }
+                else
+                {
+                    previousSelected.DeselectCandy();
+                    SelectCandy();
+                }
+            }
+        }
+    }
+    public void SwapSprite(Candy newCandy)
+    {
+        if(spriteRenderer.sprite == newCandy.GetComponent<SpriteRenderer>().sprite)
+        {
+            return;
+        }
+        Sprite oldCandy = newCandy.spriteRenderer.sprite;
+        newCandy.spriteRenderer.sprite = this.spriteRenderer.sprite;
+        this.spriteRenderer.sprite = oldCandy;
+
+        int tempId = newCandy.id;
+        newCandy.id = this.id;
+        this.id = tempId;
     }
 
-    // Update is called once per frame
-    void Update()
+    private GameObject GetNeighbor(Vector2 direction)
     {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction);
         
+        if(hit.collider != null)
+        {
+            return hit.collider.gameObject;
+        }
+        else
+        {
+            return null;
+        }
     }
-}
+    private List<GameObject> GetAllNeighbors()
+    {
+        List<GameObject> neighbors = new List<GameObject>();
+
+        foreach (Vector2 direction in adjacentDirections)
+        {
+            neighbors.Add(GetNeighbor(direction));
+        }
+        return neighbors;
+    }
+    private bool CanSwipe()
+    {
+        return GetAllNeighbors().Contains(previousSelected.gameObject);
+    }
+    private List<GameObject> FindMatch(Vector2 direction)
+    {
+        List<GameObject> matchingCandies = new List<GameObject>();
+        //consulta de los vecinos en la direccion del parametro
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction);
+        
+        while(hit.collider != null && hit.collider.GetComponent<SpriteRenderer>().sprite == spriteRenderer.sprite)
+        {
+            matchingCandies.Add(hit.collider.gameObject);
+            hit = Physics2D.Raycast(hit.collider.transform.position, direction);
+        }
+        return matchingCandies;
+    }
+    private bool ClearMatch(Vector2[] directions)
+    {
+        List<GameObject> matchingCandies = new List<GameObject>();
+
+        foreach (Vector2 direction in directions)
+        {
+            matchingCandies.AddRange(FindMatch(direction));
+        }
+        if (matchingCandies.Count >= BoardManager.MinCandiesToMatch)
+        {
+            foreach (GameObject candy in matchingCandies)
+            {
+                candy.GetComponent<SpriteRenderer>().sprite = null;
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void FindAllMatches()
+    {
+        if (spriteRenderer.sprite == null)
+        {
+            return;
+        }
+        bool hMatch = ClearMatch(new Vector2[2]
+        {
+            Vector2.left, Vector2.right
+        });
+        bool vMatch = ClearMatch(new Vector2[2]
+        {
+            Vector2.up, Vector2.down
+        });
+        if (hMatch || vMatch)
+        {
+            spriteRenderer.sprite = null;
+        }
+    }   
+}   
